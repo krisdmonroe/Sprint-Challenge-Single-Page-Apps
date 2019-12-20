3e7015ac77100572e5cef62a240ab040ca829cd5
Kris Monroe <krisdmonroe@gmail.com>
2019-12-20 11:01:34 -0800

    mvp :D thank god

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,5 +1,12 @@
 import React from "react";
+import CharacterList from "./CharacterList";
 
-export default function LocationCard({ name, type, dimension, residents }) {
-  return <span>todo: location</span>;
+export default function LocationCard({ character }) {
+  const {name, type, dimension, residents} = character
+  return <span>
+    <h1>{name}</h1>
+    <h1>{type}</h1>
+    <h1>{dimension}</h1>
+    <h1>{residents}</h1>
+  </span>;
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
+import CharacterCard from "./CharacterCard"
 
 export default function SearchForm() {
- 
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleChange = e => {
+setSearch(e.target.value)
+  }
+  useEffect(() => {
+      axios
+        .get('https://rickandmortyapi.com/api/character/')
+        .then(response => {
+          console.log(response.data.results)
+          setData(response.data.results)
+          const filtered = data.filter(character => {
+            return character.name.toLowerCase().includes(search.toLowerCase());
+          });
+          setSearchResults(filtered)
+        })
+        .catch(error => {
+          console.error('Server Error', error);
+        });    
+  }, [search]);
+  
   return (
     <section className="search-form">
-     // Add a search form here
+      <form>
+        <label htmlFor="title">Search</label>
+        <input type="text" name="title" onChange={handleChange}
+        value={search}/>
+    </form>
+  {searchResults.map(character => 
+    <CharacterCard 
+    image={character.image}
+    name={character.name}
+    species={character.species}
+    gender={character.gender}
+    />
+  )}
     </section>
   );
 }
